6d6074b43d70fddae8db232cb3a41b5b03316747
Quang Nhat-Nguyen <45399293+nnquangw@users.noreply.github.com>
2020-05-02 16:09:39 +0700

    Sidebar + icons

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import SideMenu from './components/SideMenu';
-import './Homepage.css';
+import React, { Component } from "react";
+import Sidebar from "./components/Sidebar";
+import "./Homepage.css";
 
+import HomeIcon from "@material-ui/icons/Home";
+import BarChartIcon from '@material-ui/icons/BarChart';
+import StraightenIcon from '@material-ui/icons/Straighten'
 
 class Homepage extends Component {
-
   render() {
-
     return (
-      <div className="Homepage" >
+      <div className="Homepage">
         <h1> Nguyen Nhat Quang</h1>
-        <SideMenu items={items}/>
+        <Sidebar items={items} />
       </div>
-
     );
   }
 }
+function onClick(e, item) {
+  window.alert(JSON.stringify(item, null, 2));
+}
 
 const items = [
-  { name: 'introduction', label:'Introduction' },
-  { name: 'sorting', label:'Sorting Algorithms'},
-  { name: 'piano', label:'Virtual Piano'}
-]
+  {
+    name: "introduction",
+    label: "Introduction",
+    Icon: HomeIcon,
+    items: [
+      { name: "general", label: "General Information", onClick },
+      { name: "education", label: "Education", onClick },
+      { name: "objective", label: "Objective", onClick },
+    ],
+  },
+  "divider",
+  {
+    name: "visualization",
+    label: "Algorithms Visualization",
+    Icon: BarChartIcon,
+    items: [
+      { name: "sorting", label: "Sorting Algorithms", onClick },
+      { name: "searching", label: "Searching Algorithms", onClick },
+    ],
+  },
+  "divider",
+  {
+    name: "app",
+    label: "Simulator",
+    Icon: StraightenIcon,
+    items: [
+      { name: "piano", label: "Virtual Piano" },
+    ],
+  },
+  "divider"
+];
 export default Homepage;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+// import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
+import Collapse from "@material-ui/core/Collapse";
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <List disablePadding dense>
+          {this.props.items.map((sidebarItem, index) => (
+            <React.Fragment key={`${sidebarItem.name}${index}`}>
+              {sidebarItem === "divider" ? (
+                <Divider style={{ margin: "6px 0" }} />
+              ) : (
+                <MakeSidebarItem item={sidebarItem} />
+              )}
+            </React.Fragment>
+          ))}
+        </List>
+      </div>
+    );
+  }
+}
+
+function MakeSidebarItem({
+  depthStep = 10,
+  depth = 0,
+  txtStyle = "sidebar-item-text",
+  expanded,
+  item,
+  ...rest
+}) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, items, Icon, onClick: onClickProp } = item;
+
+  function toggleCollapse() {
+    setCollapsed((prevValue) => !prevValue);
+  }
+  function onClick(e) {
+    if (Array.isArray(items)) {
+      toggleCollapse();
+    }
+    if (onClickProp) {
+      onClickProp(e, item);
+    }
+  }
+
+  let expandIcon;
+
+  if (Array.isArray(items) && items.length) {
+    expandIcon = !collapsed ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />;
+  }
+  return (
+    <>
+      <ListItem
+        className="sidebar-item"
+        onClick={onClick}
+        button
+        dense
+        {...rest}
+      >
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="sidebar-item-content"
+        >
+          {Icon && <Icon className="sidebar-item-icon" fontSize="small" />}
+          <div className={txtStyle}>{label}</div>
+        </div>
+        {expandIcon}
+      </ListItem>
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <React.Fragment key={`${subItem.name}${index}`}>
+                {subItem === "divider" ? (
+                  <Divider style={{ margin: "6px 0" }} />
+                ) : (
+                  <MakeSidebarItem
+                    depthStep={depthStep}
+                    depth={depth + 1}
+                    txtStyle={"sidebar-subitem-text"}
+                    item={subItem}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
+  );
+}
+
+const whiteNoteStyle = {
+  backgroundColor: "#ffffff",
+};
+const blackNoteStyle = {
+  backgroundColor: "#000000",
+};
+export default Sidebar;
